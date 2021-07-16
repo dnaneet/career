416b2508ba0ad345cfe27b2e2133ca40e2e70052
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2021-07-16 15:16:18 -0400

    Add files via upload

--- innovations.py
+++ innovations.py
@@ -13,7 +13,7 @@ st.set_page_config(
 
 df = pd.read_csv('innovations-timeline-2.csv')
 year_items = df['Year']
-x = st.selectbox('Select Year', np.unique(year_items))
+x = st.selectbox('Select Year', np.unique(year_items) , key = "innovations")
 st.write('My scholarly output is reflected from 2015. I was away from academia in the academic year 2015-2016, working at the French Nuclear commision.  During this time, I did not have any teaching responsibilities.')
 st.write('Teaching evaluation scores are available on request.')
 
@@ -39,6 +39,7 @@ fig1 = go.Figure(data=[go.Table(
 fig1.update_layout(width=1100, height=350)
 st.plotly_chart(fig1)
 
+
 st.title('Nomination for Instructional Awards')
 yr = np.array([2016, 2017, 2018, 2019, 2020, 2020]);
 nominations = np.array(['Wolfram Innovator Award Nomination',
@@ -60,10 +61,35 @@ fig2 = go.Figure(data=[go.Table(
             fill_color='lavender',
             align='left'))
 ])
-fig1.update_layout(width=900, height=500)
+fig2.update_layout(width=900, height=500)
 st.plotly_chart(fig2)
+
+
+st.title("Teaching Evaluation Scores")
+df_evals = pd.read_csv('TeachingEvals.csv')
+#st.dataframe(df_evals)
+yr_evals = df_evals[["Year"]]
+#x_evals = st.selectbox('Select Year', np.unique(yr_evals), key = "innovations")
+#df_evals[df_evals['Year']==x][["Course", "Enrollment", "Responded", "Eval Score out of 5.0"]]
+
+fig3 = go.Figure(data=[go.Table(
+    header=dict(values=list(df_evals[["Course", "Enrollment", "Responded", "Eval Score out of 5.0"]]),
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[df_evals[df_evals['Year']==x]['Course'],
+            df_evals[df_evals['Year']==x]['Enrollment'],
+            df_evals[df_evals['Year']==x]['Responded'],
+            df_evals[df_evals['Year']==x]['Eval Score out of 5.0']],
+            fill_color='lavender',
+            align='left'))
+])
+fig3.update_layout(width=900, height=500)
+st.plotly_chart(fig3)
+
+
+
 
 st.markdown("###### Created with Python by Aneet Narendranath, PhD")
 st.markdown('[dnaneet@mtu.edu](dnaneet@mtu.edu) | [LinkedIn](https://www.linkedin.com/in/dnaneet/) | [Google Scholar](https://scholar.google.com/citations?hl=en&user=uSSO_eAAAAAJ&view_op=list_works&authuser=1&sortby=pubdate)')
-st.markdown('[Short lecture on the Convergence, Iteration with help from my GTA](https://youtu.be/VZeUe9ZjWb8)')
+st.markdown('[Short lecture on the Convergence, Iteration with help from my GTA (Graduate Tabby Assistant)](https://youtu.be/VZeUe9ZjWb8)')
 st.markdown('[Short lecture on the importance of computational thinking](https://youtu.be/y0EJsWmNvFU)')
