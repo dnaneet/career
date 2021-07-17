f5330f0d529871c765befb8671a80f43edce0865
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2021-07-17 09:43:18 -0400

    na

--- innovations.py
+++ innovations.py
@@ -24,7 +24,7 @@ st.sidebar.markdown("Aneet Narendranath (C) 2021")
 
 df_evals = pd.read_csv('TeachingEvals.csv')
 #st.dataframe(df_evals)
-st.markdown("## :bar_chart: All Teaching Evaluation Scores Scores")
+st.markdown("## :bar_chart: All Teaching Evaluation Scores")
 st.write('Hover on each data point to see the year I instructed this course, the course enrollment and the number of students who responded to a teaching evaluation survey.')
 st.write('Heat transfer does not have data available since the paper teaching evaluation has been misplaced.  If memory serves me right, this evaluation was around 4.2 out of 5.0.')
 st.write('The bubble size is by enrollment.')
