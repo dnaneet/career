643325ac7f1690826ed8dd43465047b4713b2e01
Aneet Narendranath <dnaneet@mtu.edu>
2021-11-05 10:36:05 -0400

    CV to Portfolio

--- innovations.py
+++ innovations.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
+from PIL import Image
 #from tabulate import tabulate
 
 st.set_page_config(
@@ -22,11 +23,15 @@ df_evals = pd.read_csv('TeachingEvals.csv')
 df_evals[["Eval Score out of 5.0"]] = df_evals[["Eval Score out of 5.0"]]
 df_evals = df_evals.assign(hack='').set_index('hack')
 
+im = Image.open("career-progression.png")
+piimg = Image.open("pi.jpg")
+loginimg = Image.open("log-in.png")
+feedingimg = Image.open("feeding-habits.png")
+harppiimg = Image.open("harppi.png")
 
-
-selection = st.sidebar.radio('Select ', ["Highlights", "Teaching", "Research", "Entrepreneurial"])
+selection = st.sidebar.radio('Select ', ["Highlights", "Teaching", "Research", "Entrepreneurial", "ExtraCurricular"])
 st.sidebar.markdown("---")
-st.sidebar.write("This is an interactive page fully created in Python that showcases my teaching experience, teaching evaluation and innovations, nomination for teaching awards and course descriptions AND Research output.")
+st.sidebar.write("This interactive page fully created in Python showcases my teaching experience, innovations, nomination for teaching awards, research output, entrepreneurial efforts, and extra-curricular activities.")
 st.sidebar.markdown("Aneet Narendranath (C) 2021")
 st.sidebar.markdown("---")
 
@@ -35,26 +40,25 @@ if selection == "Highlights":
      st.title("Highlights of my Teaching, Research, Equity and Entrepreneurship")
      st.write("This page captures the highlights of my efforts since 2013 at Michigan Technological University.  Please visit in-depth pages on teaching, research and entrepreneurship by accessing the radio-buttons in the collapsible sidebar to the left.")
      st.markdown("## 🧑‍🏫 Cross-curricular Teaching with focus on collaborative problem solving studios")
-     st.write("I teach 3-4 sections of courses across the Mechanical engineering curriculum.  Since 2019, I have begun to shed the traditional classroom (inert lecturing) approach in favour of the inverted classroom, emphasis on 1-on-1 time and collaborative design studios.")
+     st.write("I teach 3-4 sections of courses, per semester, across the Mechanical engineering curriculum.  Since 2019, I have begun to shed a traditional classroom (inert lecturing) approach in favour of the inverted classroom, emphasis on 1-on-1 time, and collaborative design studios.")
      
      st.markdown("## :microscope: Multidisciplinary Research")
-     st.write("Although my full-time occupation is instruction, I am research active.  I publish 1-2 peer-reviewed papers in education research and fundamental scientific researh in peer-reviewed national and international conferences and journals.  My current research focus may be called: *the classroom as a dashboard*.  With this focus, I am actively working on numerical models and machine learning tools to support: systematic design of instruction, assessment and feedback and forecast models of student exam performance.")
+     st.write("I am research-active non tenure-track faculty.  I publish 1-2 peer-reviewed papers in education research and on fundamental scientific researh in peer-reviewed national and international conferences and journals.  My current focus is Quantitative Learning Analytics and I am actively working on numerical models and machine learning tools to support systematic design of instruction, assessment data pipelines, and forecast models of student performance.")
 
      st.markdown("## :chart_with_upwards_trend: Data-driven staffing at the Engineering Learning Center")
      st.write("I currently manage a peer-tutoring center (Engineering Learning Center) in our Mechanical engineering department.  In fall 2021, through the application of usage data gathered over the last 3 years and the Srum project management framework, I am restoring equity in salaries for our tutors while ensuring that their short-term academic and career-oriented goals are met when they work at our peer-tutoring center.  The ELC coaches were being paid 30% less than their peers at other learning centers (due to different amount of funds available).")
 
      st.markdown("## :sparkles: Entrepreneurial work")
-     st.write("In January 2021, I co-founded a tech startup with a focus on simulation sandboxes.  We were awarded a technology grant by Wolfram Research Inc.  We are currently purusing seed grants to build the scope of our technology.  In fall 2021, we are planning on supporting a *Digital Engineering* project with a senior capstone design program in North America.")
+     st.write("In January 2021, I co-founded a tech startup with a focus on simulation sandboxes.  We were awarded a technology grant by Wolfram Research Inc.  We are currently purusing seed grants to build the scope of our technology.  In fall 2021, we engaged with a Senior Capstone Design course to supporting a *Digital Engineering* project at a mid-western university in North America.  We are currently in conversations to set-up a short-term *pop-up simulation environment studio* with another university.")
 elif selection == "Teaching":
      st.title("Aneet Narendranath's Teaching Experience Dashboard")
      #st.dataframe(df_evals)
      st.markdown("## :bar_chart: All Teaching Evaluation Scores")
-     st.write('Hover on each data point to see the year I instructed this course, the course enrollment and the number of students who responded to a teaching evaluation survey.  Some of the bubbles provide additional context through an added Note.')
-     st.write('You may click and drag around data points to zoom in.')
-     st.write('Heat transfer does not have data available since the paper teaching evaluation has been misplaced.  If memory serves me right, this evaluation was around 4.2 out of 5.0.')
-     st.write('The bubble size is by enrollment.  Some bubbles are on top of each other since they represent the exact same teaching evaluation score but in different semesters.')
+     st.write('My teaching evaluation compound scores may be interactively visualized via this chart.')
+     st.write('Hover on each data point to see the year I instructed this course, the course enrollment and the number of students who responded to a teaching evaluation survey.  Some of the bubbles provide additional context through an added Note.  You may zoom into data points by  clicking and dragging around them.')
+     st.write('The bubble size signifies enrollment.  Some bubbles are on top of each other since they represent the exact same teaching evaluation score but in different semesters.')
 
-     expand_teaching_eval = st.expander(label='Expand me for a description of what these teaching evals represent and university policies governing them')
+     expand_teaching_eval = st.expander(label='Expand me for a description of how my university uses these teaching evals.')
 
      with expand_teaching_eval:
           st.markdown('##### University policies and notes about teaching evaluations:')
@@ -200,8 +204,11 @@ elif selection == "Teaching":
      st.markdown('[Video: 45 second vignette on the importance of computational thinking](https://youtu.be/y0EJsWmNvFU)')                   
 elif selection == "Research":
      st.title("Aneet Narendranath's Research Output Dashboard")
-     st.write("This is a list of publications and presentations, by year.  In the years 2016 and 2019 my presentation at the Wolfram technology conference were recorded as media and published online.")
-     st.write("My research focus has been on numerical methods for nonlinear problems in technology and quantitative methods in assessment in STEM.")
+     st.write("Computing and numerical solutions are the pillars of my research career.  I have research experience spanning multiple length and time-scales as described in this map of my activities.")
+     st.image(im, use_column_width="always")
+     st.title("Aneet Narendranath's Research Output Dashboard")
+     #st.write("In the years 2016 and 2019 my presentation at the Wolfram technology conference were recorded as media and published online.")
+     #st.write("My research focus has been on numerical methods for nonlinear problems in technology and quantitative methods in assessment in STEM.")
      df_research = pd.read_csv('ResearchCitations.csv')     
      df_research = df_research.assign(hack='').set_index('hack')
      df_research = df_research.replace({np.NAN: None})
@@ -217,6 +224,20 @@ elif selection == "Entrepreneurial":
      st.write("In March 2021 Voxel Science LLC received a technology grant from Wolfram Research Inc.")
      st.markdown("---")
      st.write("Due to potential IP, descriptions of our digital products are not immediately available for public perusal.")
+elif selection == "ExtraCurricular":
+     st.title("Aneet Narendranath's Extra-Curricular activities")	
+     st.subheader("A Raspberry-Pi learning center usage tracker")
+     st.image(piimg, use_column_width="always")
+     st.image(loginimg, use_column_width="always")
+     st.write("I wrote a bash-script that tracked the usage of a peer-supported engineering tutoring center (learning center).  This was created for a cost of approximately $100 and replaced a system that cost several thousand-dollars a year.")
+     st.write("The Mag-Pi interviewed me about this initiative.  This interview may be found via [Mag-Pi Number 65](https://www.raspberrypi.org/magpi-issues/MagPi65.pdf).")
+     st.subheader("Tracking the seasonal temperature and humidity variations around a Lyon and Healy Harp")
+     st.write("I created a Python script that was deployed on a Raspberry Pi mounted with a Pi-Hat.  The hardware mounting and soldering was performed by my spouse who is a professional harpist.  The objective of this widget was to track the temperature and relative-humidity around an expensive Lyon and Healy harp.  Sadly, this project is defunct now as I have not had the time to invest in changing the web-hosting nuances.  However, we continue to collect data using this widget and have accummulated temperature and relative-humidity data, at 5 minute intervals for 4 consecutive years!")
+     st.image(harppiimg, use_column_width='always')
+     st.subheader("Python code to visualize feeding habits of my cat")
+     st.write("In summer 2021, my cat was stricken by a severe case of IBD possibly due to intestinal lymphoma.  It was imperative that I tracked the feeding habits, patterns and quality of life of my cat so as to prolong her life through focused medical support.  I have been collecting data (manually for now) on various parameters of my cat's feeding habits and her response to an oral steroid.  We discovered that the oral steroid activates her appetite approximately 50% better than without it.  An additional discovery of a dramatic change in her feeding habits was discovered.  Pre-2021, my cat was feeding on 1-oz of solid food in the day time and 1-oz in the night.  In 2021, her feeding habits have changed to her being a trickle-feeder.  She now ingests 4-6 meals, each of 0.2 to 0.3-oz of food. My cat's veterinarian informs me that never has such a detail on a cat's feeding been captured in literature.  When time permits, I plan on scaling my effort to an open-source app with the permission and collaboration of our veterinarian.")    
+     st.image(feedingimg, use_column_width='always')
+
 
 col1, col2, col3 = st.columns(3)
 with col1:
